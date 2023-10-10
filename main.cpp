d4cc4d61e12e281c96802ae6cad7ad8e79fd407e
jaketomcko <jtomcko@gmail.com>
2023-10-10 03:37:50 -0400

    some late night coding
    * added the character sprite and some primitive movement
    * this will be turned into some more class stuff and less random variables
    * just laying out some foundations :)

--- main.cpp
+++ main.cpp
@@ -14,22 +14,46 @@ enum GameState {
 
 const int starArraySize = 15;
 
+const int windowWidth = 1280;
+const int windowHeight = 720;
+
 int main() {
 
     GameState state = RUNNING;
+    InitWindow(windowWidth, windowHeight, "swag???");
+    SetTargetFPS(120);
+
+    // make star array pog
     Star starArray[starArraySize];
     int starIndex = 0;
 
-    InitWindow(600, 400, "swag???");
-    SetTargetFPS(120);
+    // initialize sprites
+    Texture2D shipSprite = LoadTexture("sprites/ship.png");
+    int playerX = 0;
+    int playerY = 0;
 
     while(!WindowShouldClose()) {
+        
+        // fullscreen shortcut: f11
+        // if (IsKeyPressed(KEY_F11)) {
+        //     int display = GetCurrentMonitor();
 
-        if (state == RUNNING) {
-            
-        }
+        //     if (IsWindowFullscreen()) {
+        //         ToggleFullscreen();
+        //         SetWindowSize(windowWidth, windowHeight);
+        //     } 
+        //     else {
+        //         SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+        //         ToggleFullscreen();
+        //     }
+        // }
 
-        if (rand() % 120 == 0) {
+
+
+
+
+        // star generator //
+        if (rand() % 100 == 0) {
             starArray[starIndex].x = GetScreenWidth();
             starArray[starIndex].y = rand() % GetScreenHeight();
             starArray[starIndex].size = (rand() % 4) + 3;
@@ -39,18 +63,37 @@ int main() {
             }
         }
 
+        // star mover //
         for (int i = 0; i < starArraySize; i++) {
             starArray[i].move(GetFrameTime());
         }
 
-        BeginDrawing();
+        if (IsKeyDown(KEY_S)) {
+            playerY += 2;
+        }
+        if (IsKeyDown(KEY_W)) {
+            playerY -= 2;
+        }
+        if (IsKeyDown(KEY_A)) {
+            playerX -= 2;
+        }
+        if (IsKeyDown(KEY_D)) {
+            playerX += 2;
+        }
 
+        // display block
+        BeginDrawing();
+            
             ClearBackground(BLACK);
+
+            // star displayer //
             for (int i = 0; i < starArraySize; i++) {
                 if (starArray[i].x > 0) {
                     starArray[i].drawRect();
                 }
             }
+
+            DrawTexture(shipSprite, playerX, playerY, WHITE);
 
         EndDrawing();
     }
